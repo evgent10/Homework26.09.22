d7792106e656945badb3b002515f6465ce34c548
evgent10 <evgent10@ya.ru>
2022-10-01 13:09:22 +0300

    Задачи

---
+++ Задачи/Program.cs
@@ -0,0 +1,105 @@
+﻿// =====            Домашка 26.09.22
+//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и 
+//возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)      2, 4 -> 16
+// Console.WriteLine("Введите число A: ");
+// double A = double.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите число B: ");
+// double B = double.Parse(Console.ReadLine()!);
+// double resalt = Math.Pow(A, B);
+// Console.WriteLine(resalt);
+ 
+
+
+
+//=Задача 27:
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11      82 -> 10        9012 -> 12
+Console.WriteLine("Введите число: ");
+double A = double.Parse(Console.ReadLine()!);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void Method1() 
+{
+     Console.WriteLine("С уважением Евгений Ткаченко!");
+}
+Method1();
