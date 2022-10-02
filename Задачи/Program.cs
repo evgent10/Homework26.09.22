2fb509810580b53d8effd6952171064a5a4afa3b
evgent10 <evgent10@ya.ru>
2022-10-02 14:07:26 +0300

    Задачи

--- Задачи/Program.cs
+++ Задачи/Program.cs
@@ -2,21 +2,19 @@
 //Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и 
 //возводит число A в натуральную степень B.
 //3, 5 -> 243 (3⁵)      2, 4 -> 16
-Console.WriteLine("Введите число A: ");
-int A = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Введите число B: ");
-int B = int.Parse(Console.ReadLine()!);
-int result = A;
-for(int num = 1; num < B; num++)
-{
-     result = result * A;
-}
-Console.WriteLine($"{result}");
 
+// Console.WriteLine("");
+// Console.WriteLine("Введите число A: ");
+// int A = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите число B: ");
+// int B = int.Parse(Console.ReadLine()!);
+// int result = A;
+// for(int num = 1; num < B; num++)
+// {
+//      result = result * A;
+// }
+// Console.WriteLine($"{result}");
 
-
-// double resalt = Math.Pow(A, B);
-// Console.WriteLine(resalt);
  
 
 
@@ -24,6 +22,8 @@ Console.WriteLine($"{result}");
 //=Задача 27:
 // Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 // 452 -> 11      82 -> 10        9012 -> 12
+
+// Console.WriteLine("");
 // Console.Write("Введите число: ");
 // int numN = Convert.ToInt32(Console.ReadLine());
 // int SumNum(int numN)
@@ -46,7 +46,18 @@ Console.WriteLine($"{result}");
 
 //=Задача 29: 
 //Напишите программу, которая задаёт массив из 8 рандомных элементов и выводит их на экран.
-//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]               6, 1, 33 -> [6, 1, 33]
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]             6, 1, 33 -> [6, 1, 33]
+
+void GetArray(int[] array)
+{
+    for(int index = 0; index < array.Length; index++)
+    {
+        array[index] = new Random().Next(1, 100);
+        Console.Write(array[index]  + ", ");
+    }
+}
+int[] array = new int[8];
+GetArray(array);
 
 
 
@@ -112,6 +123,8 @@ Console.WriteLine($"{result}");
 
 void Method1() 
 {
-     Console.WriteLine("С уважением Евгений Ткаченко!");
+     Console.WriteLine("");
+     Console.WriteLine("С уважением, Евгений Ткаченко!");
+     Console.WriteLine("");
 }
 Method1();
